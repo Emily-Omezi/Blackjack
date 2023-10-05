9c4910a31a2181bd9c7ef6cd754115e3475f87c5
Emily-Omezi <147087507+Emily-Omezi@users.noreply.github.com>
2023-10-05 11:55:15 -0500

    Create name.py

---
+++ name.py
@@ -0,0 +1,29 @@
+def draw(n):
+    if n==1:
+        return 'Drew an Ace'
+    elif n==2:
+        return 'Drew a 2'
+    elif n==3:
+        return 'Drew a 3'
+    elif n==4:
+        return 'Drew a 4'
+    elif n==5:
+        return 'Drew a 5'
+    elif n==6:
+        return 'Drew a 6'
+    elif n==7:
+        return 'Drew a 7'
+    elif n==8:
+        return 'Drew an 8'
+    elif n==9:
+        return 'Drew a 9'
+    elif n==10:
+        return 'Drew a 10'
+    elif n==11:
+        return 'Drew a Jack'
+    elif n==12:
+        return 'Drew a Queen'
+    elif n==13:
+        return 'Drew a King'
+    else:
+        return 'BAD CARD'
